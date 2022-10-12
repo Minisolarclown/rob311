be51d6bbba8622b613f2c5829e68415bbbff3f66
minisolarclown <minisolarclown@gmail.com>
2022-10-12 19:14:45 +0100

    Added a frequency printout

--- python-examples/00_loop_execution.py
+++ python-examples/00_loop_execution.py
@@ -10,10 +10,13 @@ if __name__ == "__main__":
 
     # Print "ROB311 @UM-ROBOTICS" for 5 seconds @200Hz
     goal = 0
+    count = 0
     while(time.time() - start < EXEC_TIME):
         print("ROB311 @UM-ROBOTICS")
-        
+        count += 1
+
         goal += DT
         while(time.time() - start < goal):
             pass
     print(time.time() - start)
+    print("Frequency: " + str(count/(time.time() - start)))
