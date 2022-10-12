bc854d8d07781e524d4c2fb5e4743defdeb75a99
minisolarclown <minisolarclown@gmail.com>
2022-10-12 19:08:11 +0100

    Finished 00_loop_execution.py

--- python-examples/00_loop_execution.py
+++ python-examples/00_loop_execution.py
@@ -7,11 +7,13 @@ DT = 1/FREQ
 
 if __name__ == "__main__":
     start = time.time()
-    t = 0.0
 
     # Print "ROB311 @UM-ROBOTICS" for 5 seconds @200Hz
-
-    # YOUR
-    # CODE 
-    # GOES 
-    # HERE
+    goal = 0
+    while(time.time() - start < EXEC_TIME):
+        print("ROB311 @UM-ROBOTICS")
+        
+        goal += DT
+        while(time.time() - start < goal):
+            pass
+    print(time.time() - start)
